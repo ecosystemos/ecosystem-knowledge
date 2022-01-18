d14c6a929dfe9c6ab3817e7d51e3bce99c9948f2
albekaltay <albekaltay.aa@gmail.com>
2022-01-18 20:35:16 +0200

    ReviewCard and SliderWithReviewCard added.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import Navbar from '../src/components/Navbar'
 import Footer from './components/Footer';
 import SyllabusPage from './components/SyllabusPage';
+import SliderWithReviewCard from './components/SliderWithReviewCard';
 
 function App() {
   return (
