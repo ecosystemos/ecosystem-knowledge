34bd1773bc6f23d9cbc44cf2d12d8657f02e4820
albekaltay <albekaltay.aa@gmail.com>
2022-01-25 02:37:47 +0200

    editing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,36 @@
-import './App.css';
 import Navbar from '../src/components/Navbar'
 import Footer from './components/Footer';
 import SyllabusPage from './components/SyllabusPage';
-import SliderWithReviewCard from './components/SliderWithReviewCard';
-import NavbarForStorybook from './stories/NavbarForStorybook';
+
 
 function App() {
   return (
     <div className="App">
-      <Navbar></Navbar>
-      <SyllabusPage></SyllabusPage>
-      <Footer></Footer>
+     
+      <Navbar 
+      navbarColor={"#0E5E81"}
+      firstTab={"Syllabus"}
+      secondTab= {"Q & A"}
+      thirdTab= {"Notes"}
+      fourthTab={"Take Action"}
+      />
+
+      <SyllabusPage/>
+
+
+
+      <Footer 
+      color={"#004766"}
+      buttonText={"Sign me up!"}
+      buttonText={"Sign me up!"}
+      buttonColor={"#0E5E81"}
+      buttonWidth={"135px"}
+      buttonHeight={"40px"}          
+      />  
+          
     </div>
   );
 }
 
 export default App;
+ 
