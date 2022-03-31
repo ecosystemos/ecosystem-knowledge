fd694cb43942e04432d563415feb2cd0487fac4d
albekaltay <albekaltay.aa@gmail.com>
2022-03-31 16:04:16 +0300

    take notes settings

--- src/components/TakeNotesPage.jsx
+++ src/components/TakeNotesPage.jsx
@@ -4,6 +4,17 @@ import Navbar from './Navbar'
 import {Text} from "@chakra-ui/react"
 import Note from './Note'
 import SortAndSearch from './SortAndSearch'
+import styled from "styled-components"
+
+const TakeNotesWrapper = styled.section `
+
+margin-left: 30%;
+margin-top: 3%;
+margin-bottom: 3%;
+
+
+`;
+
 
 function TakeNotesPage() {
   return (
@@ -15,6 +26,8 @@ function TakeNotesPage() {
     thirdTab= {"Notes"}
     fourthTab={"Take Action"}
   />
+
+    <TakeNotesWrapper>
             <Text fontSize='32px'  color={"#82C6E3"}  style={{textAlign:"left", marginBottom:"16px"}}>Notes</Text>
             <Text fontSize='20px'  color="#222C2F"  style={{textAlign:"left", marginBottom:"8px"}}>Building your Startup Ecosystem</Text>
             <Text fontSize='16px'  color="#5A6D72"  style={{textAlign:"left", marginBottom:"32px"}}>Put what you learned into practice! Create and manage your tasks</Text>
@@ -26,6 +39,7 @@ function TakeNotesPage() {
     <Note></Note>
     <Note></Note>
     <Note></Note>
+    </TakeNotesWrapper>
     </React.Fragment>
   )
 }
